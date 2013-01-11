6eea6dd9f2bb771549f89d6eb35c88af0fb34f62
lilongen <lilongen@163.com>
2013-01-10 21:52:45 -0800

    basic ok

--- analyse.git.log.pl
+++ analyse.git.log.pl
@@ -13,17 +13,20 @@ use bytes;
 sub setGV();
 sub main();
 sub parse_command_line();
-sub setUserPathsChangeInfo($$);
 sub statIt($);
+sub storeCmtInfo($);
+sub richCmtFileInfoAndRetFile($);
+sub getCmtFileChangeInfo($$);
 
 my $T_SNAP		= "";
-my $CFG			= "";
-my $MID			= "";
-parse_command_line();
+my $CFG			= "example.sf.xml";
+my $MID			= "winscp.winscp3";
+#parse_command_line();
 
 my $assistor	= new Assistor($CFG, $T_SNAP); $assistor->getModules();
 my $ccvUtil 	= new CcvUtil();
-my $pms 		= $ccvUtil->loadFile($assistor->get_specified_operate_file("PMS"));
+#my $pms 		= $ccvUtil->loadFile($assistor->get_specified_operate_file("PMS"));
+my $pms = {};
 
 my $GV = {}; setGV();
 exit main();
@@ -35,7 +38,8 @@ sub setGV() {
     $GV->{AllExts} 		=~ s/(^|\s)/ \./g;
     $GV->{FileFilter}   = $GV->{ModuleInfo}->{file_filter};
     $GV->{ModuleName}   = $GV->{ModuleInfo}->{module};
-    $GV->{LogFile}      = $assistor->get_repository_log_cmd_output_file($pms->{cmt}, $GV->{ModuleInfo}->{log});
+    #$GV->{LogFile}      = $assistor->get_repository_log_cmd_output_file($pms->{cmt}, $GV->{ModuleInfo}->{log});
+	$GV->{LogFile}      = '2';
 	
     $GV->{OverallInfo}  = {
         'authorsCnt' => 0,
@@ -49,77 +53,23 @@ sub setGV() {
 }
 
 sub main() {
-	if (!open(LOG, $GV->{LogFile})) {
+	my $LOG = undef;
+	if (!open($LOG, $GV->{LogFile})) {
 		output('Can not open $GV->{LogFile}!\n');
 		
 		return 1;
 	}
 
-	my $RE = {
-		#It is followed by one or more extended header lines:
-		
-		#old mode <mode>
-		#new mode <mode>
-		#deleted file mode <mode>
-		#new file mode <mode>
-		#copy from <path>
-		#copy to <path>
-		#rename from <path>
-		#rename to <path>
-		#similarity index <number>
-		#dissimilarity index <number>
-		#index <hash>..<hash> <mode>	
-		
-		
-		#commit 9a32f12bdfcc9b7d27cd5907d838c638e5c735f7
-		#Author: Dave Methvin <dave.methvin@gmail.com>
-		#Date:   2013-01-08 01:14:01 -0800
-		#
-		#    Remove oldIE styleFloat detect.
-		#
-		#1	1	src/css.js
-		#0	4	src/support.js
-		#
-		#diff --git a/src/css.js b/src/css.js
-		#index a29356e..6b800d6 100644
-		#--- a/src/css.js
-		#+++ b/src/css.js	
-		commit => m/^commit ([\d\w]+)$/,
-		author => m/^Author (.+) <(.*)>$/,
-		date => m/^Date:\s+([\d\-]+) ([\d:]+) \-(\d+))$/,
-		comment => m/^(\s){4}(.+)$/,
-		fileLOC => m/^([\d\-]+) ([\d\-]+) (.+)$/,
-		fileDiffHeader => m/^diff --git a\/(.+) b\/(.+)$/,
-		changeMode => m/^(old mode|new mode|deleted file mode|new file mode|copy from|copy to|rename from|rename to) (.+)$/,
-		similarity => m/^(similarity index) (.+)$/,
-		dissimilarity => m/^(dissimilarity index) (.+)$/,
-		changeIndexAndMode => m/^index (\w+)\.\.(\w+) (.+)$/,
-		oldMode => m/^old mode(.+)$/,
-		newMode => m/^new mode(.+)$/
-	};
-
     my $lineCnter = 0;
-	my $doCmtTest = 1;
 	my $cmtCnter = 0;
     my $flags = {}; resetFlags($flags);
 	
     my $cmtInfo;
-	my $fileInfo;
-	my $authorInfo;
-	
+	my $cmtFileInfo;
     my $line;
     my $prevLine;
-	my $diffingFile;
-	while (1) {
-		$line = <LOG>;
-		if (!defined($line)) {
-			if ($lineCnter == 1) {
-				$GV->{Error} = $prevLine;
-			}
-			
-			last;
-		}
-		
+	while (!eof($LOG)) {
+		$line = <$LOG>;
 		$lineCnter++;
 		$prevLine = $line;
 		#print $line;
@@ -176,27 +126,59 @@ sub main() {
 #+# author: lilong'en(lilongen@163.com)
 #+# date:   04/05/2010
 
-		if ($flags->{doCmtReTest}) {
-			if ($line =~ $RE->{commit}) {
-				$flags->{cmtFound} = 1;
+#commit => m/^commit ([\d\w]+)$/,
+#author => m/^Author (.+) <(.*)>$/,
+#date => m/^Date:\s+([\d\-]+) ([\d:]+) \-(\d+)$/,
+#comment => m/^(\s){4}(.+)$/,
+#fileLOC => m/^([\d\-]+) ([\d\-]+) (.+)$/,
+#fileDiffHeader => m/^diff --git a\/(.+) b\/(.+)$/,
+#changeMode => m/^(old mode|new mode|deleted file mode|new file mode|copy from|copy to|rename from|rename to) (.+)$/,
+#similarity => m/^(similarity index) (.+)$/,
+#dissimilarity => m/^(dissimilarity index) (.+)$/,
+#changeIndexAndMode => m/^index (\w+)\.\.(\w+) (.+)$/,
+#oldMode => m/^old mode(.+)$/,
+#newMode => m/^new mode(.+)$/
 
+		#print Dumper($flags);
+
+		if ($flags->{doCmtReTest}) {
+			if ($line =~ m/^commit ([\d\w]+)$/) {
+print "cmtFound: $1 \n";
 				$cmtCnter++;
+				if ($cmtCnter > 1) {
+					richCmtFileInfoAndRetFile({
+						flags => $flags,
+						LOG => $LOG,
+						line => $line,
+						cmtInfo => $cmtInfo
+					});
+					
+					storeCmtInfo($cmtInfo);
+					resetFlags($flags);
+				}
+				
+				$flags->{doCmtReTest} = 0;
+				$flags->{cmtFound} = 1;
+				
 				$cmtInfo = {};
 				$cmtInfo->{cmt} = $1;
+				next;
 			}
-			next;
 		}
 		
-		if ($line =~ $RE->{author}) {
-			$flags->{doCmtReTest} = 0;
+		if ($flags->{cmtFound} && $line =~  m/^Author: (.+) <(.*)>$/) {
+print "authorFound: $1 \n";		
 			$flags->{authorFound} = 1;
+			$flags->{cmtFound} = 0;
 			$cmtInfo->{authorName} = $1;
 			$cmtInfo->{authorEmail} = $2;
 			next;
 		}
 		
-		if ($flags->{authorFound} && $line =~ $RE->{date}) {
+		if ($flags->{authorFound} && $line =~ m/^Date:\s+([\d\-]+) ([\d:]+) \-(\d+)$/) {
+print "dateFound: $1 \n";			
 			$flags->{dateFound} = 1;
+			$flags->{authorFound} = 0;
 			$cmtInfo->{date} = $1;
 			$cmtInfo->{time} = $2;
 			$cmtInfo->{timeZone} = $3;
@@ -204,27 +186,34 @@ sub main() {
 		}
 		
 		if ($flags->{dateFound} && $line =~ m/^$/) {
+print "commentBeginFound:\n";		
 			$flags->{commentBeginFound} = 1;
+			$flags->{dateFound} = 0;
 			$cmtInfo->{comment} = '';
 			next;
 		}
-		
-		if ($cmtInfo->{commentBeginFound}) {
+
+		if ($flags->{commentBeginFound}) {
+
 			if ($line =~ m/^$/) {
+print "cmtFilesBeginFound: \n";			
 				$flags->{cmtFilesBeginFound} = 1;
 				$flags->{commentBeginFound} = 0;
 				$cmtInfo->{arrayFiles} = [];
 				$cmtInfo->{hashFiles} = {};
 			} else {
-				$cmtInfo->{comment} .= $line;
+				# m/^(\s){4}(.+)$/
+print "comment: $line\n";		
+				$cmtInfo->{comment} .= substr($line, 4);
 			}
 			next;
 		}
 		
 		if ($flags->{cmtFilesBeginFound}) {
-			if ($line =~ $RE->{fileLOC}) {
+			if ($line =~ m/^([\d\-]+)\t([\d\-]+)\t(.+)$/) {
 				$cmtFileInfo = {};
 				$cmtFileInfo->{file} = $3;
+print "cmt changed file:  $3\n";				
 				$cmtFileInfo->{addLines} = $1;
 				$cmtFileInfo->{delLines} = $2;
 				$cmtFileInfo->{cmt} = $cmtInfo->{cmt};
@@ -232,37 +221,95 @@ sub main() {
 				$cmtInfo->{hashFiles}->{$cmtFileInfo->{file}} = $cmtFileInfo;
 			} else {
 				$flags->{filesDiffBegin} = 1;
+print "filesDiffBegin:  \n";				
 				$flags->{cmtFilesBeginFound} = 0;
 			}
 			next;
 		}
 		
 		if ($flags->{filesDiffBegin}) {
-			if ($line =~ $RE->{fileDiffHeader}) {
-				my $aFile = $1;
-				my $bFile = $2;
-				my $changeInfo = getCmtFileChangeInfo($aFile, $bFile);
-				$cmtInfo->{hashFiles}->{$changeInfo->{file}}->{mode} = $changeInfo->{mode};
+			if ($line =~ m/^diff --git a\/(.+) b\/(.+)$/) {
+print "diff --git:  \n";			
+				$flags->{prevDiffFile} = richCmtFileInfoAndRetFile({
+					aFile => $1,
+					bFile => $2,
+					flags => $flags,
+					LOG => $LOG,
+					line => $line,
+					cmtInfo => $cmtInfo
+				});
+				
 				$flags->{doCmtReTest} = 1;
 			}
+			next;
 		}
 	}
 	
-	close(LOG);
+	if ($flags->{prevDiffFile}) {
+		richCmtFileInfoAndRetFile({
+			flags => $flags,
+			LOG => $LOG,
+			line => undef,
+			cmtInfo => $cmtInfo
+		});
+		storeCmtInfo($cmtInfo);
+		resetFlags($flags);
+	}
 
-	$ccvUtil->dumpFile($assistor->getSvnModuleLogParsedInfoFile($pms, $MID), {
-	    'OverallInfo' => $GV->{OverallInfo},
-		'AuthorsInfo' => $GV->{AuthorsInfo},
-		'CmtsInfo'  => $GV->{CmtsInfo},
-		'PathsInfo' => $GV->{PathsInfo},
-		'PathPaths' => $GV->{PathPaths},
-		'Error' 	=> $GV->{Error}
-	});
+	close($LOG);
+	
+	if ($lineCnter == 1) {
+		$GV->{Error} = $prevLine;
+	}
+
+	#$ccvUtil->dumpFile('git.log.ccv', {
+	#    'OverallInfo' => $GV->{OverallInfo},
+	#	'AuthorsInfo' => $GV->{AuthorsInfo},
+	#	'CmtsInfo'  => $GV->{CmtsInfo},
+	#	'FilesInfo'  => $GV->{FilesInfo},
+	#	'Error' 	=> $GV->{Error}
+	#});
+	
+	$assistor->write_file('git.log.ccv', Dumper($GV->{CmtsInfo}));
 
 	return 0;
 }
 
-sub getCmtFileChangeInfo(my $aFile, $bFile) {
+sub storeCmtInfo($) {
+	my $cmtInfo = $_[0];
+	$GV->{CmtsInfo}->{$cmtInfo->{cmt}} = $cmtInfo;
+}
+
+sub richCmtFileInfoAndRetFile($) {
+	my $po = $_[0];
+	my $aFile 	= $po->{aFile};
+	my $bFile 	= $po->{bFile};
+	my $flags 	= $po->{flags};
+	my $LOG 	= $po->{LOG};
+	my $line 	= $po->{line};
+	my $cmtInfo = $po->{cmtInfo};
+	
+	if (defined($flags->{prevDiffFile})) {
+		if (!defined($line)) {
+			$cmtInfo->{hashFiles}->{$flags->{prevDiffFile}}->{offsetE} = tell($LOG);
+		} else {
+			$cmtInfo->{hashFiles}->{$flags->{prevDiffFile}}->{offsetE} = tell($LOG) - length($line) - 1;
+		}
+	}
+
+	if (defined($aFile)) {
+		my $changeInfo = getCmtFileChangeInfo($aFile, $bFile);
+		$cmtInfo->{hashFiles}->{$changeInfo->{file}}->{changeMode} = $changeInfo->{mode};	
+		$cmtInfo->{hashFiles}->{$changeInfo->{file}}->{offsetB} = tell($LOG) - length($line);
+		return $changeInfo->{file};
+	} else {
+		return undef;
+	}
+}
+
+sub getCmtFileChangeInfo($$) {
+	my $aFile = $_[0];
+	my $bFile = $_[1];
 	my $mode = 'normal';
 	my $file = $aFile;
 	if ($aFile eq '/dev/null') {
@@ -272,9 +319,10 @@ sub getCmtFileChangeInfo(my $aFile, $bFile) {
 	if ($bFile eq '/dev/null') {
 		$mode = 'deleted';
 	}
+	
 	return {
-		mode: $mode,
-		file: $file
+		mode => $mode,
+		file => $file
 	};
 }
 
@@ -295,7 +343,8 @@ sub resetFlags($) {
 	$flags->{dateFound} = 0;
 	$flags->{commentBeginFound} = 0;
 	$flags->{cmtFilesBeginFound} = 0;
-	$flags->{filesDiffBegin} = 0;	
+	$flags->{filesDiffBegin} = 0;
+	$flags->{prevDiffFile} = undef;
 }
 
 sub parse_command_line() {
